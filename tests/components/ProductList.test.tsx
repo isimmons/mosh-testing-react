3c44138528e15d40b4dbcff108e7bba03d771d25
isimmons <isimmons33@gmail.com>
2024-03-07 06:14:06 -0500

    add test for error ProductList

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -35,4 +35,11 @@ describe("ProductList", () => {
 
     expect(message).toBeInTheDocument();
   });
+
+  it("should render an error message when there is an error", async () => {
+    server.use(http.get("/products", () => HttpResponse.error()));
+    render(<ProductList />);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
 });
