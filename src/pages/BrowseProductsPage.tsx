a8937f68b364aaf6e0a3346f9d8a3ce1d9181a99
isimmons <isimmons33@gmail.com>
2024-03-09 16:48:55 -0500

    extract CategorySelect

--- src/pages/BrowseProductsPage.tsx
+++ src/pages/BrowseProductsPage.tsx
@@ -1,18 +1,14 @@
-import { Select, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import axios, { AxiosError } from "axios";
 import { useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import QuantitySelector from "../components/QuantitySelector";
-import { Category, Product } from "../entities";
 import { useQuery } from "react-query";
+import CategorySelect from "../components/CategorySelect";
+import QuantitySelector from "../components/QuantitySelector";
+import { Product } from "../entities";
 
 function BrowseProducts() {
-  const categoriesQuery = useQuery({
-    queryKey: ["categories"],
-    queryFn: () => axios.get<Category[]>("/categories").then((res) => res.data),
-  });
-
   const productsQuery = useQuery<Product[], AxiosError>({
     queryKey: ["products"],
     queryFn: () => axios.get<Product[]>("/products").then((res) => res.data),
@@ -26,39 +22,6 @@ function BrowseProducts() {
   // cause a blank page with nothing but the error. I think it needs to go.
   // all tests pass after removing.
   // if (productsQuery.error) return <div>Error: {productsQuery.error.message}</div>;
-
-  const renderCategories = () => {
-    const { data: categories, isLoading, error } = categoriesQuery;
-    if (isLoading)
-      return (
-        <div role="progressbar" aria-label="loading categories">
-          <Skeleton />
-        </div>
-      );
-
-    if (error) return null;
-
-    return (
-      <Select.Root
-        onValueChange={(categoryId) =>
-          setSelectedCategoryId(parseInt(categoryId))
-        }
-      >
-        <Select.Trigger placeholder="Filter by Category" />
-        <Select.Content>
-          <Select.Group>
-            <Select.Label>Category</Select.Label>
-            <Select.Item value="all">All</Select.Item>
-            {categories?.map((category) => (
-              <Select.Item key={category.id} value={category.id.toString()}>
-                {category.name}
-              </Select.Item>
-            ))}
-          </Select.Group>
-        </Select.Content>
-      </Select.Root>
-    );
-  };
 
   const renderProducts = () => {
     const { data: products, error, isLoading } = productsQuery;
@@ -115,7 +78,11 @@ function BrowseProducts() {
   return (
     <div>
       <h1>Products</h1>
-      <div className="max-w-xs">{renderCategories()}</div>
+      <div className="max-w-xs">
+        <CategorySelect
+          onChange={(categoryId) => setSelectedCategoryId(categoryId)}
+        />
+      </div>
       {renderProducts()}
     </div>
   );
