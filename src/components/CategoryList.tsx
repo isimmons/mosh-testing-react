37c158f13733baea10f4c16ae65ab2d5de085bbb
isimmons <isimmons33@gmail.com>
2024-03-14 03:53:36 -0400

    convert to react-query and remove redux from project

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,28 +1,30 @@
-import { useEffect } from "react";
-import { fetchCategories } from "../store/categorySlice";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useQuery } from "react-query";
+import axios, { AxiosError } from "axios";
+import { Category } from "../entities";
 
 function CategoryList() {
-  const dispatch = useAppDispatch();
-  const categories = useAppSelector((state) => state.category.list);
-  const loading = useAppSelector((state) => state.category.loading);
-  const error = useAppSelector((state) => state.category.error);
+  const {
+    data: categories,
+    isLoading,
+    error,
+  } = useQuery<Category[], AxiosError>({
+    queryKey: ["categories"],
+    queryFn: () => axios.get<Category[]>("/categories").then((res) => res.data),
+  });
 
-  useEffect(() => {
-    dispatch(fetchCategories());
-  }, [dispatch]);
-
-  if (error) return <div>Error: {error}</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
       <h2>Category List</h2>
-      {loading ? (
+      {isLoading ? (
         <div>Loading...</div>
       ) : (
         <ul>
-          {categories!.map((category) => (
-            <li key={category.id}>{category.name}</li>
+          {categories?.map((category) => (
+            <li key={category.id} aria-label={category.name}>
+              {category.name}
+            </li>
           ))}
         </ul>
       )}
--- tests/AllProviders.tsx
+++ tests/AllProviders.tsx
@@ -1,7 +1,7 @@
+import { Theme } from "@radix-ui/themes";
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { CartProvider } from "../src/providers/CartProvider";
-import { Theme } from "@radix-ui/themes";
 
 const AllProviders = ({ children }: PropsWithChildren) => {
   const client = new QueryClient({
